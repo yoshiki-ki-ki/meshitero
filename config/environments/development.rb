50ee59321839dbd49785fac593fc2190548e11a1
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-09 10:15:02 +0000

    [Finish]1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   
   config.hosts << "bac28c463ebd431a821bef218a815aec.vfs.cloud9.us-east-1.amazonaws.com"
+  
+  config.active_job.queue_adapter = :inline
+  
 end
