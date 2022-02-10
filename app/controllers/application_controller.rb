188245e516d165129f333aa3ea2e76b34c2f7dad
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-10 11:31:13 +0000

    [Finish] 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  #サインイン後にaboutへ遷移
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  
+  #サインアウト後にaboutへ遷移
+  def after_sign_out_path_for(resource)
+    about_path
+  end
 
   protected
 
