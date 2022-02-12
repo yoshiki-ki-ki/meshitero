30484d12b55d0d12152ddf102e47fa5d07d58900
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-12 10:45:37 +0000

    [Finish] 11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   
   #サインイン後にaboutへ遷移
   def after_sign_in_path_for(resource)
-    about_path
+    post_images_path
   end
   
   #サインアウト後にaboutへ遷移
