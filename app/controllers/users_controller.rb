3982ec6528a64e5945b331b379ef6c854c8dfbeb
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-02-13 11:01:04 +0000

    [Finish] 15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-  def edit
-  end
 
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+  end
+  
+  def edit
   end
 end
